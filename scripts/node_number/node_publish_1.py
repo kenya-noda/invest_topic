a339c836791d6e76f4dc6bd19493ad2f5ee84c7c
k.noda <k.noda@a.phys.nagoya-u.ac.jp>
2019-02-21 11:55:29 +0900

    non topic

--- scripts/node_number/node_publish_1.py
+++ scripts/node_number/node_publish_1.py
@@ -8,16 +8,15 @@ import std_msgs.msg
 
 if __name__=="__main__":
     rospy.init_node("publisher")
-    #num = rospy.get_param("~n")
-    """
+
     topic_to = rospy.Publisher(
-            name = "node_check{}".format(num),
+            name = "node_check0",
             data_class = std_msgs.msg.Float64,
             queue_size = 1,
             )
-    """
+
     while not rospy.is_shutdown():
         t = time.time()
-        #topic_to.publish(t)
+        topic_to.publish(t)
         time.sleep(0.1)
         continue
--- scripts/node_number/node_subscribe_ex.py
+++ scripts/node_number/node_subscribe_ex.py
@@ -7,22 +7,23 @@ import std_msgs.msg
 sys.path.append("/home/amigos/python/n2lite/")
 import n2lite
 
-
+"""
 def callback(req):
     time_sub = time.time()
     
     time_hensa = time_sub - req.data
     return
+"""
 
 if __name__=="__main__":
     rospy.init_node("sub_ex")
-    num = rospy.get_param("~n")
-
+    #num = rospy.get_param("~n")
+    """
     topic_from = rospy.Subscriber(
             name = "node_check{}".format(num),
             data_class = std_msgs.msg.Float64,
             callback = callback,
             queue_size = 1,
             )
-
+    """
     rospy.spin()
