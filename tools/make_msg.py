7996bab4e95d6749342c0fa1d869d2edff61dcc0
kenya-noda <k.noda@a.phys.nagoya-u.ac.jp>
2019-03-01 06:50:32 +0000

    fix

--- tools/make_msg.py
+++ tools/make_msg.py
@@ -3,7 +3,7 @@
 import subprocess
 import time
 
-cmd = "python process-msg.py --node_num {0} --topic_num {1} --msg {2}"
+cmd = "python process-msg.py --node_num {0} --topic_num {1}"
 node_list = [10, 40, 70, 100]
 topic_list = [1, 2, 6, 10, 14, 18, 20]
 
